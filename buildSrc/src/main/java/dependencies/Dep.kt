7d9c7d365843ca8a024cfe79c97357bfa0822123
ttanaka330 <tomyamkun330@gmail.com>
2021-12-12 22:36:55 +0900

    Add Jetpack Compose

--- buildSrc/src/main/java/dependencies/Dep.kt
+++ buildSrc/src/main/java/dependencies/Dep.kt
@@ -2,25 +2,36 @@ package dependencies
 
 object Dep {
 
+    object Versions {
+        const val compose = "1.0.5"
+        const val kotlin = "1.5.31"
+    }
+
     object Kotlin {
         const val coroutines = "org.jetbrains.kotlinx:kotlinx-coroutines-android:1.5.2"
         const val serialization = "org.jetbrains.kotlinx:kotlinx-serialization-json:1.3.1"
     }
 
-    object Google {
-        const val material = "com.google.android.material:material:1.4.0"
-    }
-
     object Jetpack {
         const val appcompat = "androidx.appcompat:appcompat:1.4.0"
-        const val constraintlayout = "androidx.constraintlayout:constraintlayout:2.1.2"
         const val core = "androidx.core:core-ktx:1.7.0"
-        const val livedata = "androidx.lifecycle:lifecycle-livedata-ktx:2.4.0"
-        const val viewmodel = "androidx.lifecycle:lifecycle-viewmodel-ktx:2.4.0"
+    }
+
+    object Compose {
+        const val activity = "androidx.activity:activity-compose:1.3.1"
+        const val navigation = "androidx.navigation:navigation-compose:2.4.0-alpha10"
+        const val viewModel = "androidx.lifecycle:lifecycle-viewmodel-compose:1.0.0-alpha07"
+
+        const val material = "androidx.compose.material:material:${Versions.compose}"
+        const val materialIcons = "androidx.compose.material:material-icons-extended:${Versions.compose}"
+        const val ui = "androidx.compose.ui:ui:${Versions.compose}"
+        const val uiTooling = "androidx.compose.ui:ui-tooling:${Versions.compose}"
+
+        const val coil = "io.coil-kt:coil-compose:1.4.0"
     }
 
     object Koin {
-        private const val koinVersion = "3.1.3"
+        private const val koinVersion = "3.1.4"
         const val android = "io.insert-koin:koin-android:$koinVersion"
         const val compose = "io.insert-koin:koin-androidx-compose:$koinVersion"
         const val navigation = "io.insert-koin:koin-androidx-navigation:$koinVersion"
